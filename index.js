fbb1b3b0b2e9c8a637b53485caf5fe5e957c839e
Jonasaa00 <jonasaarhaug@gmail.com>
2022-01-16 19:57:38 +0100

    update

---
+++ index.js
@@ -0,0 +1,38 @@
+// question 1
+
+
+var pets =["cat 5.5", "dog 3.8", "parrot 4.0"]
+
+console.log (pets[0]);
+console.log (pets[1]);
+console.log (pets[2]);
+
+for (i =0; i < pets.age; i++) {
+    if(cars[i].year < 4){
+        document.write(cars[i].make + ", " + pets[i].make)
+    }
+}
+
+
+// question 2
+function myFunction (x, y) {
+    if (y===undefined) {
+        y=2;
+    }
+}
+console.log (myFunction)
+
+
+// question 3
+
+
+fruits.sort();
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"]
+fruits.pop();
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"]
+fruits.shift();
+
+console.log (fruits);
+
